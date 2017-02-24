f84dd9333ad93c4ee9a4966452696a843521e40a
internousdev <internousdev@00174045111207>
2017-02-24 15:18:52 +0900

    struts2更新

---
+++ src/com/internousdev/struts2/action/LoginAction.java
@@ -0,0 +1,36 @@
+package com.internousdev.struts2.action;
+
+public class LoginAction extends ActionSupport implements SessionAware {
+private String name;
+private String password;
+private Map<String, Object> session;
+public String execute() {
+String ret = ERROR;
+LoginDAO dao = new LoginDAO();
+LoginDTO dto = new LoginDTO();
+dto = dao.select(name,password);
+if(name.equals(dto.getName())) {
+if(password.equals(dto.getPassword()))｛
+ret = SUCCESS ;
+}
+}
+session.put(“name”, dto.getName());
+return ret;
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getPassword() {
+return password;
+}
+public void setPassword(String password) {
+this.password = password;
+}
+public Map<String, Object> getSession() {
+return session;
+}
+public void setSession(Map<String, Object> session) {
+this.session = session;
+}
---
+++ src/com/internousdev/struts2/dto/LoginDTO.java
@@ -0,0 +1,29 @@
+package com.internousdev.struts2.dto;
+
+public class LoginDTO {
+	private int id;
+	private String name;
+	privateString password;
+
+
+	public int getID(){
+		return id;
+
+	}
+
+	public void setId(int id){
+		this.id=id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getPassword(){
+		return passsword;
+	}
+
+	public void setPassword(String password){
+		this.password=password;
+	}
+}
